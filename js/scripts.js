6554dc489b4279c2ad66d611daf13cb7568722d3
Jeff Drury <me@jeffdrury.com>
2017-01-10 06:11:45 -0500

    More map styling updates

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $(document).foundation();
 function initMap() {
   var uluru = {lat: 41.737406, lng: -71.323055};
   var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
+      zoom: 12,
       center: uluru
   });
   var marker = new google.maps.Marker({
